03e49fc222b56645a4d0f9d6b3ac1103580665b0
Erik Mejia <erikdojo352@gmail.com>
2023-10-19 13:39:37 -0400

    style changes based on correct guess

--- script.js
+++ script.js
@@ -19,10 +19,19 @@ document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
+  //When there is no input
   if (!guess) {
     document.querySelector('.message').textContent = '⛔️ No Number';
+
+    //When the playes guesses the correct number
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number!';
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+
+    document.querySelector('.number').style.width = '30rem';
+
+    //When player guesses too high
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '📈 Too High';
@@ -33,6 +42,8 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent = '💣 You Lost the Game';
       document.querySelector('.score').textContent = 0;
     }
+
+    //When pleayer guesses too low
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '📉 Too Low..';
